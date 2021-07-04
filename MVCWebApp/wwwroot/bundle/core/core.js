028f8c2f090ac0733ed9e6902be8b248b8b60225
vule <vule5726@gmail.com>
2021-07-04 13:04:37 +0300

    fix missing file

--- MVCWebApp/wwwroot/bundle/core/core.js
+++ MVCWebApp/wwwroot/bundle/core/core.js
@@ -26,14 +26,45 @@ return /******/ (() => { // webpackBootstrap
   \*********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module './sass/core.scss'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n\n\n//# sourceURL=webpack://mvcwebapp/./src/core.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sass_core_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./sass/core.scss */ \"./src/sass/core.scss\");\n\n\n//# sourceURL=webpack://mvcwebapp/./src/core.js?");
+
+/***/ }),
+
+/***/ "./src/sass/core.scss":
+/*!****************************!*\
+  !*** ./src/sass/core.scss ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://mvcwebapp/./src/sass/core.scss?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/make namespace object */
@@ -52,8 +83,7 @@ eval("__webpack_require__.r(__webpack_exports__);\nObject(function webpackMissin
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/core.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/core.js");
 /******/ 	
 /******/ 	return __webpack_exports__;
 /******/ })()
